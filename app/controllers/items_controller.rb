075232242e6279cdd7269902a4445c9d66d8b83d
Dawna Ara <dawnaara@gmail.com>
2016-08-15 14:45:14 -0700

    created _form and _item.html.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
 
-def create
-	@user = User.find(params[:user_id])
-	@item = @user.items.build(item_params)
-	@item.user = current_user
+  def create
+    @user = User.find(params[:user_id])
+    @item = @user.items.build(item_params)
+    @item.user = current_user
 
-     if @item.save
-       flash[:notice] = "Item was added to list."
-       redirect_to [@user, @item]
-     else
+    if @item.save
+      flash[:notice] = "Item was added to list."
+      redirect_to [@user, @item]
+    else
 
-       flash.now[:alert] = "There was an error adding your item. Please try again."
-       render :new
-     end
-   end
-
+      flash.now[:alert] = "There was an error adding your item. Please try again."
+      render :new
+    end
+  end
 end
 
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show]  
+  resources :users, only: [:show] do 
   	resources :items, only: [:create]
+  end
 
   root to: 'welcome#index'
 end
