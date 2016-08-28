57ad69157a91e2f5fb1d03e367bd1fc9c40f2d9d
Dawna Ara <dawnaara@gmail.com>
2016-08-28 13:58:03 -0700

    updated _items and show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_users
 
   def create
     @item = current_user.items.build(item_params)
@@ -11,10 +12,33 @@ class ItemsController < ApplicationController
     redirect_to current_user
   end
 
+  def destroy
+    @item = @user.items.find(params[:item_id])
+    if @item.destroy
+      flash[:notice] = "Item was deleted."      
+    else
+      flash.now[:alert] = "There was an error deleting your item. Please try again."      
+    end
+
+    redirect_to current_user
+
+  end
+
+  def complete
+    @item.update_attribute(:completed_at, Time.now)
+
+    redirect_to current_user, notice: "Item completed!"
+  end
+
   private 
+
+  def set_users
+    @users = User.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name)
   end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-  	resources :items, only: [:create, :destroy]
+  	resources :items, only: [:create, :destroy] do
+  		member do
+  			delete :destroy
+  		end
+  	end
   end
 
   root to: 'welcome#index'
